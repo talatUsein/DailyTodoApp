dbb1a0898089416ece4d4394934d5e8f771df585
Talat-Usein <taliat575@gmail.com>
2022-07-16 14:39:57 +0300

    test runner

---
+++ src/test/java/org/DailyTodo/runners/testRunner.java
@@ -0,0 +1,21 @@
+package org.DailyTodo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+        plugin = {"json:target/cucumber.json" ,
+                "html:target/default_html_reports",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false
+)
+
+
+public class testRunner {
+
+}

9053f45f3595e334d1623f60788dd1c0f818f1f5
Talat-Usein <taliat575@gmail.com>
2022-07-16 15:10:14 +0300

    Text area and Save tasks button WebElements were added

--- src/test/java/org/DailyTodo/pages/TaskCreationPage.java
+++ src/test/java/org/DailyTodo/pages/TaskCreationPage.java
@@ -1,4 +1,20 @@
 package org.DailyTodo.pages;
 
+import org.DailyTodo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class TaskCreationPage {
+
+    public TaskCreationPage () {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(tagName = "textarea")
+    public WebElement textInputBox;
+
+    @FindBy(xpath = "//p/input[@type='submit']")
+    public WebElement saveTasksButton;
+
 }
